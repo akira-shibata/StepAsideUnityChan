ead0040ab5a820b2bb03ae45059997a2929c0d98
akira-shibata <eita.love67@gmail.com>
2019-01-05 10:28:17 +0900

    完了。

---
+++ Assets/DestroyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour {
+
+	private GameObject unityChan;
+
+	// Use this for initialization
+	void Start () {
+		this.unityChan = GameObject.Find ("unitychan");
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.unityChan.transform.position.z > this.transform.position.z + 5.0){
+				Destroy(this.gameObject);
+			}
+	}
+}
